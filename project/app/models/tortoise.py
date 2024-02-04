54565b4231bf57acce70dd2f0a38875b226da367
Brian Keane <lonesomewhistle@gmail.com>
2024-02-03 18:26:27 -0600

    postgres setup

---
+++ project/app/models/tortoise.py
@@ -0,0 +1,8 @@
+from tortoise import fields, models
+
+class Client(models.Model):
+    client_id = fields.UUIDField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
