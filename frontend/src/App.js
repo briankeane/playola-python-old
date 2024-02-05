903ae04e43ed64f00f7c0f48906af3a868623c79
Brian Keane <lonesomewhistle@gmail.com>
2024-02-05 17:38:25 -0600

    add react app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const SPOTIFY_CLIENT_ID = "-----------------------------";
+  const REDIRECT_URI = "http://localhost:3000/spotifyRedirect";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href={`${AUTH_ENDPOINT}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
