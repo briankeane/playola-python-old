783cf576aa1621fb606a7d8cabf256257da4b19c
Brian Keane <lonesomewhistle@gmail.com>
2024-02-05 18:08:18 -0600

    pytest setup

---
+++ project/app/api/tests/test_spotify_tokens.py
@@ -0,0 +1,6 @@
+
+
+def test_ping(test_app):
+    response = test_app.get("/healthcheck")
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "responds": True, "testing": True}
