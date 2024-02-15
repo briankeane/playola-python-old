e9a7293eb78133e6c366949c6b4b34fea4d0d719
Brian Keane <lonesomewhistle@gmail.com>
2024-02-14 18:14:32 -0600

    fix react build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const SPOTIFY_CLIENT_ID = "-----------------------------";
-  const REDIRECT_URI = "http://localhost:3000/spotifyRedirect";
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
-
   return (
     <div className="App">
       <header className="App-header">
