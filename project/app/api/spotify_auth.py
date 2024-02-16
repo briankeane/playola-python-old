542c1ae7767552d689f66c6faa50cf5aa1485ad0
Brian Keane <lonesomewhistle@gmail.com>
2024-02-16 16:26:50 -0600

    add client_base_url to python app

--- project/app/api/spotify_auth.py
+++ project/app/api/spotify_auth.py
@@ -28,7 +28,7 @@ async def spotifyAuthCode(code: str, settings: Settings = Depends(get_settings))
     sp_oauth = oauth2.SpotifyOAuth(
         settings.spotify_client_id,
         settings.spotify_client_secret,
-        "http://localhost:8004/v1/auth/spotify/code",
+        "{settings.client_base_url}/v1/auth/spotify/code",
         scope=scopes,
         cache_path=".spotipyoauthcache",
     )
--- project/app/config.py
+++ project/app/config.py
@@ -9,12 +9,13 @@ log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     environment: str = "dev"
-    base_url: str = "http://localhost:8004/"
+    base_url: str = "http://localhost:8004"
     testing: bool = bool(0)
     database_url: AnyUrl = None
     spotify_client_id: str = "SPOTIFY_CLIENT_ID"
     spotify_client_secret: str = "SPOTIFY_CLIENT_SECRET"
     spotify_redirect_uri: str = f"{base_url}/v1/auth/spotify/code"
+    client_base_url: str = "http://localhost:3000"
 
 
 @lru_cache
