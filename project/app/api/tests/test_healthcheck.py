c6856902b266dd4cd62c73165b3fc54f70b0bc55
Brian Keane <lonesomewhistle@gmail.com>
2024-02-16 16:58:58 -0600

    ok more

--- project/app/api/tests/test_healthcheck.py
+++ project/app/api/tests/test_healthcheck.py
@@ -4,4 +4,10 @@ from app import main
 def test_ping(test_app):
     response = test_app.get("/healthcheck")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "responds": True, "testing": True}
+    assert response.json() == {
+        "environment": "dev",
+        "responds": True,
+        "testing": True,
+        "base_url": "http://localhost:8004",
+        "client_base_url": "http://localhost:3000",
+    }
