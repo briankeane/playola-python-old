5870d013d0191144248fe1f4989eab4caa2be491
Brian Keane <lonesomewhistle@gmail.com>
2024-02-16 16:56:29 -0600

    ok more

--- project/app/api/healthcheck.py
+++ project/app/api/healthcheck.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends
-
 from app.config import Settings, get_settings
+from fastapi import APIRouter, Depends
 
 router = APIRouter()
 
@@ -11,4 +10,6 @@ async def healthcheck(settings: Settings = Depends(get_settings)):
         "responds": True,
         "environment": settings.environment,
         "testing": settings.testing,
+        "client_base_url": settings.client_base_url,
+        "base_url": settings.base_url,
     }
